5ef1097a0ba580966709cf27b003fe72ef91239c
lee mighdoll <lee@underneath.ca>
2025-05-20 21:42:38 -0700

    simplify example, just show the linked string

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,15 @@
 /// <reference types="wesl-plugin/suffixes" />
-import appCode from "../shaders/app.wesl?static";
+import code from "../shaders/app.wesl?static";
 
 main();
 
 async function main(): Promise<void> {
-  displayShaderCode(appCode);
-
-  launchShader(appCode);
+  displayShaderCode(code);
 }
 
-function displayShaderCode(wgslSrc: string): void {
+function displayShaderCode(src: string): void {
   const app = document.querySelector<HTMLDivElement>("#app");
   if (app) {
-    app.innerHTML = `<pre>${wgslSrc}<pre>`;
+    app.innerHTML = `<pre>${src}<pre>`;
   }
-}
-
-async function launchShader(code: string): Promise<void> {
-  const adapter = await navigator.gpu.requestAdapter();
-  const device = await adapter?.requestDevice();
-  if (!device) {
-    console.error("Failed to create GPU device");
-    return;
-  }
-  const module = device.createShaderModule({ code });
-
-  const pipeline = device.createComputePipeline({
-    layout: "auto",
-    compute: { module },
-  });
-
-  const commands = device.createCommandEncoder();
-  const pass = commands.beginComputePass();
-  pass.setPipeline(pipeline);
-  pass.dispatchWorkgroups(1, 1, 1);
-  pass.end();
-  device.queue.submit([commands.finish()]);
 }
